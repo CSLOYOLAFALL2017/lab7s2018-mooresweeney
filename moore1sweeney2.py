65bb3f61570ed4ed07f6f895755d07c7de871ce0
Bmoore <bcmoore@loyola.edu>
2018-03-20 12:03:58 -0400

    version 1

---
+++ moore1sweeney2.py
@@ -0,0 +1,28 @@
+
+
+def error():
+    filename = input("please enter a file name: ")
+    file_not_found = True  # get inside loop at least once
+    while (file_not_found):
+        try:
+
+            file = open(filename, "r")
+            fileNotFound = False  # do not get back in loop
+            # read from file
+            return file
+
+
+        except FileNotFoundError:  # ask for file name again
+            filename = input("Enter a file name: ")
+
+
+
+def profit(file):
+    for line in file:
+        date, name, budget, revenue = line.split(",")
+        print(name)
+
+
+
+nameOfFile = error()
+profit(nameOfFile)
